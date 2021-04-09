82ed9965a8a1995cdafbecf0f67655652730ffc2
acpn <ant.pnasc@gmail.com>
2021-04-08 23:01:42 -0300

    Adding post page

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -2,10 +2,11 @@ import Head from 'next/head';
 import { GetStaticProps } from 'next';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
+import Link from 'next/link';
 
-import { getPrismicClient } from '../services/prismic';
+import { getPrismicClient } from '../../services/prismic';
 
-import styles from '../styles/pages/posts.module.scss';
+import styles from './posts.module.scss';
 
 type Post = {
   slug: string;
@@ -29,11 +30,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
