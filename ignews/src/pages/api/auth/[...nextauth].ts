c1822459664885afcfd8218714ddb274a8d35dfc
acpn <ant.pnasc@gmail.com>
2021-04-14 10:24:46 -0300

    updating whole repo

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,9 @@
-import { query as q } from 'faunadb'
+import { query as q } from 'faunadb';
 
-import NextAuth from 'next-auth'
-import { session } from 'next-auth/client'
-import Providers from 'next-auth/providers'
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
 
-import { fauna } from '../../../services/fauna'
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -16,24 +15,40 @@ export default NextAuth({
   ],
   callbacks: {
     async session(session) {
-      const userActiveSubscription = await fauna.query(
-        q.Get(
-          q.Match(
-            q.Index('subscription_by_user_ref'),
-            q.Select(
-              "ref",
-              q.Get(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(session.user.email)
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
                 )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                'active'
               )
-            )
+            ])
           )
-        )
-      )
+        );
 
-      return session;
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        }
+      }
     },
     async signIn(user, account, profile) {
       const { email } = user
