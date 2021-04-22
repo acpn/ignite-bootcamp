52765e0c1eba72300aafdefe9a0655f5e4ab2795
Antonio Nascimento <acpn@Antonios-MacBook-Air.local>
2021-04-22 11:21:59 -0300

    Finishing the app

--- dashgo/src/components/Sidebar/SidebarNav.tsx
+++ dashgo/src/components/Sidebar/SidebarNav.tsx
@@ -14,13 +14,13 @@ export function SidebarNav() {
     <Stack spacing="12" align="flex-start">
 
       <NavSection title="GERAL">
-        <NavLink icon={RiDashboardLine} children="Dashboard" />
-        <NavLink icon={RiContactsLine} children="Usuários" />
+        <NavLink icon={RiDashboardLine} children="Dashboard" href="/dashboard" />
+        <NavLink icon={RiContactsLine} children="Usuários" href="/users" />
       </NavSection>
 
       <NavSection title="AUTOMAÇÃO">
-        <NavLink icon={RiInputMethodLine} children="Formulários" />
-        <NavLink icon={RiGitMergeLine} children="Automação" />
+        <NavLink icon={RiInputMethodLine} children="Formulários" href="/forms" />
+        <NavLink icon={RiGitMergeLine} children="Automação" href="/automation" />
       </NavSection>
     </Stack>
   );
